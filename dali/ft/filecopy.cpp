73435232cf62c11260098041ae3874b8054fb8f7
Attila Vamos <attila.vamos@gmail.com>
2014-05-27 21:47:12 +0100

    HPCC-10666 Operations on Files should not, by default, change the state of the file. e.g. STD.File.Copy converts compressed files to uncompressed files.
    Add fix.
    
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- dali/ft/filecopy.cpp
+++ dali/ft/filecopy.cpp
@@ -2697,6 +2697,10 @@ void FileSprayer::spray()
     if ((sourceSize == 0) && failIfNoSourceFile)
         throwError(DFTERR_NoFilesMatchWildcard);
 
+    LOG(MCdebugInfo, job, "compressedInput:%d, compressOutput:%d", compressedInput, compressOutput);
+    if (compressedInput && !compressOutput)
+        compressOutput = true;
+
     LocalAbortHandler localHandler(daftAbortHandler);
 
     if (allowRecovery && progressTree->getPropBool(ANcomplete))
